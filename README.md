2ef416ef8664e204b51d46d3be0fa9026706e8b6
Ant_M84 <anthony.l.michel@gmail.com>
2023-09-30 17:33:12 -0400

    updated README.md, refined script.js

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Random-Password-Generator
-Accepts user input on several variables to generate a random password of a specified length
+
+## Description
+
+This application will generate a random password based on user input on varied prompts.
+
+## Usage
+
+1. Open the application in a web brower.
+2. Upon clicking the 'Generate Password' button, you will be presented with the option to choose a numerical password length between 8 and 128 characters.  If you choose a value outside of this range, you will be prompted to enter a value within the specified range and the prompt will reset.  If you cancel the prompt, the alert "Cancelled" will display and the function will end.
+3. You will then be advised that you have four character choices to include in the password, of which at least one option MUST be selected in order to generate a valid password.  If no selections are made an invalid result will display.
+4. Via separate consecutive prompts, you will be asked if you would like to include lowercase, uppercase, numbers and special characters respectively.
+5. Based on your chosen criteria, a random password will be generated of the selected length and displayed in the box.
+
+## Credits
+
+This application was created via source code (index.html, style.css and script.js) received from the University of North Carolina: Chapel Hill Coding Bootcamp syllabus.  Only the script.js file has been modified with code added to enable a functioning application.
+
+Assistance with developing code and debugging was obtained from:
+
+Vega, D. (2020) Random Password Generator in JavaScript.
+https://www.youtube.com/watch?v=v2jfGo7ztm8&ab_channel=DanielVega
+
+## Application Link
+
+![Alt text](application-image.png)
+
--- script.js
+++ script.js
@@ -3,36 +3,14 @@ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O","P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = ["!", "?", "#", "*"];
-var randompassword = [];
-
+var randomPassword = [];
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Write password to the #password input
-function writePassword() {
-  var acceptChoice = userChoice();
-    if(acceptChoice) {
-      var password = generatePassword();
-      var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-    }
-  }
-
-function generatePassword() { 
-  var password = "";
-  for(var i = 0; i < passwordLength; i++) {
-    var randomvalue = Math.floor(Math.random() * randompassword.length);
-    password = password + randompassword[randomvalue];
-  }
-  return password;
-}
-
-function userChoice() {
-  randompassword = []
+// Function to generate password length and content based on user input and assign input to 'randomPassword' array.  'parseInt' used to convert string to number. 'isNan' used to verify valid input of number and specify range - if not function is cancelled. Alert to inform user of required input to receive a valid result. //
+function userInput() {
+  randomPassword = []
   passwordLength = parseInt(prompt("Please enter a numerical value of desired password length between 8 and 128 characters."));
    if (!passwordLength) {
     alert("Cancelled!")
@@ -40,22 +18,40 @@ function userChoice() {
    } else
     if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
       alert("You must enter a numerical value between 8 and 128!");
-      return userChoice();
+      return userInput();
    }
+      alert("You will now be prompted to choose from four character types to include in your password.  You must select AT LEAST ONE option to generate a valid password.") 
    if (confirm("Would you like to include lowercase characters?")) {
-    randompassword = randompassword.concat(alphabet);
+    randomPassword = randomPassword.concat(alphabet);
    }
    if (confirm("Would you like to include uppercase characters?")) {
-    randompassword = randompassword.concat(caps);
+    randomPassword = randomPassword.concat(caps);
    }
     if (confirm("Would you like to include numbers?")) {
-    randompassword = randompassword.concat(number);
+    randomPassword = randomPassword.concat(number);
    }
     if (confirm("Would you like to include special characters?")) {
-    randompassword = randompassword.concat(special);
+    randomPassword = randomPassword.concat(special);
    }
    return true;
+}
+// Function to generate password based on a loop accessing the user input data (as stored in 'randomPassword' array) to return a password based on the loop parameters //
+function generatePassword() { 
+  var password = "";
+  for(var i = 0; i < passwordLength; i++) {
+    var randomValue = Math.floor(Math.random() * randomPassword.length);
+    password = password + randomPassword[randomValue];
   }
+  return password;
+}
+// Write password to the #password input
+// Will use the password result from 'generatePassword' function if 'userOpions' function (user input) returns a valid result //
+function writePassword() {
+    userInput();
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
 
-// https://www.youtube.com/watch?v=v2jfGo7ztm8&ab_channel=DanielVega //
+
 
